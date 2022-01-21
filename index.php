7e4fea506205125275033a0bb74aea54ec872dca
Parvathy-AB-Nair <77499701+Parvathy-AB-Nair@users.noreply.github.com>
2022-01-21 13:14:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<html>
+<title>Scrappage Policy Automation</title>
+<head>
+   <link rel="stylesheet" type="text/css" href="css/index.css">
+   <link rel="shortcut icon" href="img/chase.jpg">
+    </head>
+
+<body>
+<?php include'header.php'?>
+<div class="index_container">
+    <div class="slider">
+    <div class="slideimg">
+    <img src="img/img.png">
+        <img src="img/img.png">
+        <img src="img/img.png">
+        <img src="img/img.png">
+        <img src="img/img.png">
+        </div>
+    </div><br>
+    <!-- <div class="newsroom">
+
+        <marquee class="marquee_news" scrolldelay="20"><p class="newsfeed"><span>Your bank may charge you for closing a bank account.
+        Bank also charges you when you close your account within a particular time period.</span><span>SBI cuts deposit rates; PPF to fetch lower interest rate. </span><span>No, it is not mandatory to link your bank account with CITIZENSHIP card</span></p></marquee>
+    </div><br><br> -->
+
+
+
+<div class="news_events">
+    <h4>Tips | Updates | Events</h4><br>
+        <ul>
+            <p>First, SignUp/Register. Then login and scan your RC book QR code.
+ </p><br>
+<p>And finally, apply for scrappage of your vehicle.
+
+</p>
+            <br>
+        </ul>
+    </div>
+
+
+    <div class="online_services">
+        <h4>SignUp/Login</h4>
+        <ul>
+
+          <a href="#" id="ebanking" ><li><div class="ebanking">Customer
+              <div class = "ebanking_options">
+              <ul>
+                  <a href="customer_login.php"><li>Login </li></a>
+                  <a href="ebanking_reg_form.php"><li>Register</li></a>
+              </ul>
+          </div>
+
+    <a href="#" id="ebanking" ><li><div class="ebanking">Godown Manager
+        <div class = "ebanking_options">
+        <ul>
+            <a href="customer_login.php"><li>Login </li></a>
+            <a href="ebanking_reg_form.php"><li>Register</li></a>
+        </ul>
+    </div>
+
+        </ul>
+
+</div>
+
+        <div id="aboutus" class="about"><span>About Policy</span><br><br>
+        <p>The Vehicle Scrappage Policy, launched on August 13, 2021, is a government-funded programme to replace old vehicles with modern & new vehicles on Indian roads. The policy is expected to reduce pollution, create job opportunities and boost demand for new vehicles.According to the new policy, commercial vehicles aged >15 years and passenger vehicles aged >20 years will have to be mandatorily scrapped if they do not pass the fitness and emission tests.
+</p></div>
+
+    
+
+    </div>
+
+<?php include 'footer.php';?>
+</body>
+
+</html>
